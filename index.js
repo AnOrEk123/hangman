1f1fc99c05a2a2dc0e8eb95c302987a6b5d53771
Web <web@it.step>
2022-06-27 19:54:09 +0300

    reading-input

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const words = ['dog', 'flower', 'car', 'rocket', 'home', 'brush', 'tea', 'mouse', 'fork', 'plane'];
+
+document.getElementById('button').onclick = letter
+
+function letter() {
+    let letter = document.getElementById('input').value
+    console.log(letter)
+}
